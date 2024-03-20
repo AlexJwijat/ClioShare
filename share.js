49c22d742fed82f6b1ff8c1f3796f5e4c1f0b066
Alex <ajwijat@ajayit.com>
2024-03-19 18:10:00 -0700

    Added button appearance functionality, email subject, user identification, etc.

--- share.js
+++ share.js
@@ -1,32 +1,45 @@
 // ==UserScript==
 // @name         Clio Manage - Share Multiple File Links
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      2.2
 // @description  Send links of selected files in Clio Manage
-// @match        https://app.clio.com/*
+// @match        https://app.clio.com/nc/*
 // @grant        GM_addStyle
 // @updateURL    https://github.com/AlexJwijat/ClioShare/raw/main/share.js
 // @downloadURL  https://github.com/AlexJwijat/ClioShare/raw/main/share.js
-
 // ==/UserScript==
+
 (function() {
     'use strict';
 
     // Function to get the selected file links
     function getSelectedFileLinks() {
+        // outlook formatting fix
+        const minLength = 120;
         return [...document.querySelectorAll(".row-selection-checkbox > .checked")].map(e => {
-            const documentLink = e.parentElement.parentElement.querySelector(".name-column-line__item__name > a");
-            const documentId = documentLink.getAttribute('href').split('/')[4];
-            const documentTitle = documentLink.title;
+            const documentLink = e.parentElement.parentElement.querySelector(".name-column-line__item__name");
+            const documentId = documentLink.id;
+            const documentTitle = documentLink.querySelector("a").title.trim();
             const documentUrl = `https://app.clio.com/nc/#/documents/${documentId}/details`;
-            return `${documentTitle}: ${documentUrl}`;
+            const line = `${documentTitle}: ${documentUrl}`;
+            return line + " ".repeat(Math.max(0, minLength - line.length));;
         }).join("\n");
+    }
+
+    // Function to retrieve the username
+    function getUsername() {
+        const elem = document.querySelector("[data-view-component='user-menu'] span");
+        return elem ? elem.textContent : "";
     }
 
     // Function to open the email application with the selected file links
     function sendSelectedFileLinks() {
         const selectedLinks = getSelectedFileLinks();
-        const emailSubject = "Shared";
+        console.log("wtf");
+        console.log(selectedLinks);
+        console.log("???");
+        const username = getUsername();
+        const emailSubject = `${username} has shared these files with you`;
         const emailBody = selectedLinks;
         const mailtoLink = `mailto:?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`;
         window.location.href = mailtoLink;
@@ -46,6 +59,7 @@
     sendButton.style.borderRadius = '4px';
     sendButton.style.cursor = 'pointer';
     sendButton.style.zIndex = '9999';
+    sendButton.style.display = 'none';
     sendButton.addEventListener('click', sendSelectedFileLinks);
     document.body.appendChild(sendButton);
 
@@ -55,4 +69,15 @@
             background-color: #45a049;
         }
     `);
+
+    setInterval(function() {
+        const checkElems = document.querySelectorAll(".row-selection-checkbox > .checked");
+        if (location.href.includes("documents") && checkElems && checkElems.length > 0){
+            sendButton.style.display = 'block';
+        } else {
+            sendButton.style.display = 'none';
+        }
+    }, 100);
+
+
 })();
